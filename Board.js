a268eadf0323e27f74100ea1926544e20f4a5a4b
Angel2217 <53708383+Angel2217@users.noreply.github.com>
2019-09-24 11:46:05 +0200

    Update Board.js

--- Board.js
+++ Board.js
@@ -49,16 +49,16 @@ export class Board {
     placePlayer() {
         let playerCount = 0;
         while (playerCount < 2) {
-            let r1 = Math.floor(Math.random() * this.size);
+            let r1 = Math.floor(Math.random() * this.size); // this part is too repetitive?
             let r2 = Math.floor(Math.random() * this.size);
             let c1 = Math.floor(Math.random() * this.size);
             let c2 = Math.floor(Math.random() * this.size);
-            if (Math.abs(c2 - c1) > 1 || Math.abs(r2 - r1) > 1) {
+            if (Math.abs(c2 - c1) > 1 || Math.abs(r2 - r1) > 1) { // this is to make sure the players are not close together
                 let tdId1 = `sq_${r1}_${c1}`;
                 let square1 = Square.getById(tdId1);
                 let tdId2 = `sq_${r2}_${c2}`;
                 let square2 = Square.getById(tdId2);
-                if (!square1.blocked && $('#' + tdId1).children().length === 0 &&
+                if (!square1.blocked && $('#' + tdId1).children().length === 0 && // I still have this problem, if I try to check !square1.player, I get errors: unrecognized expression # or unrecognized expression [object object]  
                     !square2.blocked && $('#' + tdId2).children().length === 0) {
                     square1.player = this.players[0];
                     square2.player = this.players[1];
@@ -76,7 +76,7 @@ export class Board {
             let c = Math.floor(Math.random() * this.size);
             let tdId = `sq_${r}_${c}`;
             let square = Square.getById(tdId);
-            if (!square.blocked && $('#' + tdId).children().length === 0) {
+            if (!square.blocked && $('#' + tdId).children().length === 0) { // same problem as above
                 let w = this.weapons.pop();
                 square.weapon = w;
                 weaponCount++;
