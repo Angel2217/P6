16efa144b4cf6b7ecec96c3c8c4009400f155677
Angel2217 <53708383+Angel2217@users.noreply.github.com>
2019-10-02 10:43:37 +0200

    Update 02/10

--- App.js
+++ App.js
@@ -1,34 +1,50 @@
 import { Board } from './Board.js';
 import { Player } from './Player.js';
 import { Weapon } from './Weapon.js';
+import { Panel } from './Panel.js';
+
+
+const players = [new Player('Player1',
+        'alien.png'),
+    new Player('Player2',
+        'astronaut.png')
+];
+
+const weapons = [new Weapon('Asteroid',
+        'asteroid.png',
+        20),
+    new Weapon('Comet',
+        'comet.png',
+        15),
+    new Weapon('Fighter',
+        'fighter.png',
+        10),
+    new Weapon('Phaser',
+        'phaser.png',
+        5)
+];
 
 
 export class App {
     constructor(size) {
+        this.size = size;
+        this.panels = this.initPanel();
         this.board = this.initBoard(size);
     }
 
+
+    initPanel() {
+        this.panels = [new Panel('Panel1', players[0]),
+                     new Panel('Panel2', players[1])
+        ];
+        return this.panels;
+    }
+
+    
     initBoard(size) {
-        const players = [new Player('Player1',
-                '<img src="images/alien.png">'),
-            new Player('Player2',
-                '<img src="images/astronaut.png">')
-        ];
-        const weapons = [new Weapon('Asteroid',
-                '<img src="images/asteroid.png">',
-                20),
-            new Weapon('Comet',
-                '<img src="images/comet.png">',
-                15),
-            new Weapon('Fighter',
-                '<img src="images/fighter.png">',
-                10),
-            new Weapon('Phaser',
-                '<img src="images/phaser.png">',
-                5)
-        ];
         this.board = new Board(size, players, weapons);
         return this.board;
     }
+
 
 }
