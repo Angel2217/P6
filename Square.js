6a85a1b637a26495312439710fb3c7936756d727
Angel2217 <53708383+Angel2217@users.noreply.github.com>
2019-10-08 11:34:57 +0200

    Update 08/10

--- Square.js
+++ Square.js
@@ -2,79 +2,101 @@ import { Player } from './Player.js';
 import { Weapon } from './Weapon.js';
 
 export class Square {
-     constructor(id) {
-         this.id = id;
-     }
+    constructor(id) {
+        this.id = id;
+    }
 
-     get blocked() {
-         let elem = $('#' + this.id);
-         return $(elem).hasClass('blocked');
-     }
+    get blocked() {
+        let elem = $('#' + this.id);
+        return $(elem).hasClass('blocked');
+    }
 
-     set blocked(bool) {
-         let elem = $('#' + this.id);
-         if (bool) {
-             $(elem).addClass('blocked');
-         } else {
-             $(elem).removeClass('blocked');
-         }
-     }
+    set blocked(bool) {
+        let elem = $('#' + this.id);
+        if (bool) {
+            $(elem).addClass('blocked');
+        } else {
+            $(elem).removeClass('blocked');
+        }
+    }
 
-     get player() {
-         let p = null;
-         let td = $('#' + this.id + ' .player');
-         if (td.length > 0) {
-             let elem = td[0];
-             let name = $(elem).attr('id');
-             p = new Player(name);
-         }
-         return p
-     }
-
-
-     set player(p) {
-         let td = $('#' + this.id)[0]; 
-         if (p === null) {
-             $('.player', td).remove();
-         } else {
-             $(td).append(p.elem); 
-         }
-     }
-
-     get weapon() {
-         let w = null;
-         let td = $('#' + this.id + ' .weapon');
-         if (td.length > 0) {
-             let elem = td[0];
-             let name = $(elem).attr('id');
-             let image = $('.weapon-image', elem).html();
-             let damage = $('.damage', elem).html();
-             w = new Weapon(name, image, damage);
-         }
-         return w
-     }
-
-     set weapon(w) {
-         let td = $('#' + this.id)[0]; 
-         if (w === null) {
-             $('.weapon', td).remove();
-         } else {
-             $(td).append(w.elem); 
-         }
-     }
-
-    
-     static getById(id) {
-         return new Square(id);
-     }
+    get player() {
+        let p = null;
+        let td = $('#' + this.id + ' .player');
+        if (td.length > 0) {
+            let elem = td[0];
+            let name = $(elem).attr('id');
+            p = new Player(name);
+        }
+        return p
+    }
 
 
-     get location() {
+    set player(p) {
+        let td = $('#' + this.id)[0];
+        if (p === null) {
+            $('.player', td).remove();
+        } else {
+            $(td).append(p.elem);
+        }
+    }
+
+    get weapon() {
+        let w = null;
+        let td = $('#' + this.id + ' .weapon');
+        if (td.length > 0) {
+            let elem = td[0];
+            let name = $(elem).attr('id');
+            let image = $('.weapon-image', elem).html();
+            let damage = $('.damage', elem).html();
+            w = new Weapon(name, image, damage);
+        }
+        return w
+    }
+
+
+    set weapon(w) {
+        let td = $('#' + this.id)[0];
+        if (w === null) {
+            $('.weapon', td).remove();
+        } else {
+            $(td).append(w.elem);
+        }
+    }
+
+
+    static getByLocation(r, c) {
+        return new Square(`sq_${r}_${c}`);
+    }
+
+
+    get location() {
         let loc = {
             row: Number((this.id)[3]),
             col: Number((this.id)[5])
         }
         return loc
-     }
+    }
 
- } 
+
+    static getPlayerSquare() {
+        let tdId = $('.player').parent().attr('id');
+        return new Square(tdId);
+    }
+
+
+    get active() {
+        let elem = $('#' + this.id);
+        return $(elem).hasClass('active');
+    }
+
+    set active(bool) {
+        let elem = $('#' + this.id);
+        if (bool) {
+            $(elem).addClass('active');
+        } else {
+            $(elem).removeClass('active');
+        }
+    }
+
+}
