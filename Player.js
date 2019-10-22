fb9a7723cebf4555fc5361a8c7212e97c149c4b3
Angel2217 <53708383+Angel2217@users.noreply.github.com>
2019-10-22 14:48:52 +0200

    Update Player.js
    Appended this.weapon.elem and added changeWeapon() method.

--- Player.js
+++ Player.js
@@ -1,48 +1,53 @@
 export class Player {
-    constructor(name, image, points, weapon) {
-      this.name = name;
-      this.image = image;
-      this.points = points;
-      this.weapon = weapon;
-    
-  
-  
-      let myDiv = $('#' + name); 
-      if (myDiv.length === 0) {
-        this.elem = this._createElem(); 
-      } else {
-        this.elem = myDiv[0]; 
-      }
-  
+  constructor(name, image, points, weapon) {
+    this.name = name;
+    this.image = image;
+    this.points = points;
+    this.weapon = weapon;
+
+
+
+    let myDiv = $('#' + name);
+    if (myDiv.length === 0) {
+      this.elem = this._createElem();
+    } else {
+      this.elem = myDiv[0];
     }
 
-  
-    _createElem() {
-      let elem = $('<div>')
-        .attr('id', this.name)
-        .addClass('player')
-        .append('<div class="player-name">' + this.name + '</div>')
-        .append('<div class="player-image">' + `<img src = images/${this.image}>` + '</div>')
-        .append('<div class="player-points">' + this.points + '</div>')
-        .append('<div class="player-weapon">' + this.weapon + '</div>');
-      return elem
-    }
-    
-    
-    get active() {
-        let elem = $('#' + this.name);
-        return $(elem).hasClass('active');
-    }
+  }
 
-    set active(bool) {
-        let elem = $('#' + this.name);
-        if (bool) {
-            $(elem).addClass('active');
-        } else {
-            $(elem).removeClass('active');
-        }
+
+  _createElem() {
+    let elem = $('<div>')
+      .attr('id', this.name)
+      .addClass('player')
+      .append('<div class="player-name">' + this.name + '</div>')
+      .append('<div class="player-image">' + `<img src = images/${this.image}>` + '</div>')
+      .append('<div class="player-points">' + this.points + '</div>')
+      .append(this.weapon.elem);
+    return elem
+  }
+
+
+  get active() {
+    let elem = $('#' + this.name);
+    return $(elem).hasClass('active');
+  }
+
+  set active(bool) {
+    let elem = $('#' + this.name);
+    if (bool) {
+      $(elem).addClass('active');
+    } else {
+      $(elem).removeClass('active');
     }
+  }
   
-    
-    
-  } 
+
+  changeWeapon(prevWeapon, newWeapon) {
+    let p = $('#' + this.name);
+    this.weapon = newWeapon;
+    $(p).remove(prevWeapon.elem).append(newWeapon.elem);
+  }
+
+}
